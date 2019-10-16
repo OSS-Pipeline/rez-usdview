d5ebf74cded833b6a4d09f4cf00c3b99b978eba7
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-10-16 14:07:30 -0400

    Updated the build process to make it possible to use usd_core and usdview as separated projects, with a working pxr Python module

--- package.py
+++ package.py
@@ -15,7 +15,7 @@ description = \
 requires = [
     "cmake-3+",
     "gcc-6+",
-    "usd-{version}".format(version=str(version))
+    "usd_core-{version}".format(version=str(version)),
 ]
 
 variants = [
